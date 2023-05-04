01ea6c4c2706b24655da853be90f48682f3ad848
fajarbudi <fajarbudira@gmail.com>
2023-05-05 00:19:05 +0700

    mengubah layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 "use client";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
-import AOS from "aos";
-import "aos/dist/aos.css";
 const inter = Montserrat({ weight: "300", subsets: ["latin"] });
-
-export const metadata = {
-  title: "Elektronika",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  AOS.init();
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
