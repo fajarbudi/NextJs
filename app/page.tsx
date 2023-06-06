56975feb64bfe04a001dc83b73bfdca73f9aa494
fajarbudi <fajarbudira@gmail.com>
2023-06-06 21:19:05 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,18 @@
-"use client"
 import Navbar from "./navbar";
 import Tebalpapan from "../component/TebalPapan";
 import DuckResonator from "../component/Duckresonator";
 import LenghtPort from "../component/Lenghtport";
 import BoxSpeaker from "../component/Boxspeaker";
-import {useEffect} from "react"
+import Scrolling from "./scrolling"
 export const metadata = {
   title: "Elektronika-Halaman 1",
   description: "",
 };
 export default function halaman1() {
-  useEffect(() =>{
-    window.addEventListener("scroll", () =>{
-      const scrollDwon = document.querySelector(".scrollDown")
-      const scrollUp = document.querySelector(".scrollUp")
-      const y = window.scrollY
-      const Heigth = document.body.scrollHeight - window.innerHeight + 40
-
-      scrollDwon?.classList.add("-ml-20")
-      setTimeout(() =>{
-      scrollDwon?.classList.remove("-ml-20")
-      },500)
-
-      if (Heigth == y){
-        scrollDwon?.classList.add("opacity-0")
-        scrollUp?.classList.remove("opacity-0")
-      } else {
-        scrollDwon?.classList.remove("opacity-0")
-        scrollUp?.classList.add("opacity-0")
-      }
-    })
-  })
     return (
       <>
         <Navbar judul="Menghitung Box Speaker" />
-        <div className="scrollDown"> Scroll  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down-circle" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg> </div>
-        <div className="scrollUp opacity-0"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up-circle" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/> </svg> Scroll</div>
+        <Scrolling/>
         <section>
           <Tebalpapan />
           <DuckResonator />
---
+++ app/scrolling.jsx
@@ -0,0 +1,32 @@
+"use client"
+import { useEffect } from "react"
+export default function scrolling(){
+    useEffect(() =>{
+        window.addEventListener("scroll", () =>{
+          const scrollDwon = document.querySelector(".scrollDown")
+          const scrollUp = document.querySelector(".scrollUp")
+          const y = window.scrollY
+          const Heigth = document.body.scrollHeight - window.innerHeight + 40
+    
+          scrollDwon?.classList.add("-ml-20")
+          setTimeout(() =>{
+          scrollDwon?.classList.remove("-ml-20")
+          },500)
+    console.log(y)
+    console.log(Heigth)
+          if (Heigth == y){
+            scrollDwon?.classList.add("opacity-0")
+            scrollUp?.classList.remove("opacity-0")
+          } else {
+            scrollDwon?.classList.remove("opacity-0")
+            scrollUp?.classList.add("opacity-0")
+          }
+        })
+      })
+    return(
+        <>
+        <div className="scrollDown"> Scroll  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down-circle" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg> </div>
+        <div className="scrollUp opacity-0"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up-circle" viewBox="0 0 16 16"> <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/> </svg> Scroll</div>
+        </>
+    )
+}
